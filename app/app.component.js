0b3861286e8ddd6cbacf7df7c3e31d79d20312d3
Carl Warburton <CarlWarburton@Carls-MacBook-Pro.local>
2017-01-13 11:01:51 +1100

    link documents and homepage

--- app/app.component.js
+++ app/app.component.js
@@ -16,7 +16,7 @@ var AppComponent = (function () {
         core_1.Component({
             moduleId: module.id,
             selector: 'app',
-            template: "\n\n    <h1>Navigation</h1>\n    \n    <router-outlet></router-outlet>\n    "
+            templateUrl: 'app.component.html'
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
